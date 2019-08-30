67cda2f9d831fb4b26ade879a8cb2fad3745aa66
kry-dev <yaroslavkryvda@gmail.com>
2019-08-30 12:23:47 +0300

    add what-is section

---
+++ src/blocks/modules/what-is/what-is.js
@@ -0,0 +1,13 @@
+modules.define('what-is', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
