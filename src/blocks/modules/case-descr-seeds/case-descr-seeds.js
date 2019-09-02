b53223a6b87cc617d265ba6ba88c420462a44c1c
kry-dev <yaroslavkryvda@gmail.com>
2019-09-03 00:40:57 +0300

    seeds page

---
+++ src/blocks/modules/case-descr-seeds/case-descr-seeds.js
@@ -0,0 +1,13 @@
+modules.define('case-descr-seeds', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
