06693da2ae0e7678da990ddc250120a2edb38270
kry-dev <yaroslavkryvda@gmail.com>
2019-09-02 20:04:08 +0300

    add get in touch page

---
+++ src/blocks/modules/get-in-touch/get-in-touch.js
@@ -0,0 +1,13 @@
+modules.define('get-in-touch', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/hero-getintouch/hero-getintouch.js
@@ -0,0 +1,13 @@
+modules.define('hero-getintouch', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
